b038534a4bf987c2d492161e4b32a0bbfada26bd
Vladimir noboa <vladimirillichnoboap@gmail.com>
2019-02-04 20:52:48 -0500

    login_page

---
+++ Lands/Infrastructure/InstanceLocator.cs
@@ -0,0 +1,24 @@
+﻿
+namespace Lands.Infrastructure
+{
+    using ViewModels;
+    using System;
+
+    public class InstanceLocator
+    {
+        #region Properties
+        public MainViewModel Main
+        {
+            get;
+            set;
+        }
+        #endregion
+
+        #region Constructors
+        public InstanceLocator()
+        {
+            this.Main = new MainViewModel();
+        }
+        #endregion
+    }
+}
